7dd1c6b8ac5af1d33eb1a83742dc9e0e3ec9c45d
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2024-06-08 07:58:16 -0300

    rotas protegidas

--- src/router.js
+++ src/router.js
@@ -23,20 +23,20 @@ router.post('/login', authController.login)
 
 // usuarios
 router.get('/user', authMiddleware.authenticateToken, userController.getAll);
-router.post('/registerUser', userMiddleware.checkRequiredFieldsToRegister, userMiddleware.checkUserExists, userController.createUser);
-router.put('/updateUser/:id', userMiddleware.checkRequiredFieldsToRegister, userController.updateUser);
+router.post('/registerUser', authMiddleware.authenticateToken, userMiddleware.checkRequiredFieldsToRegister, userMiddleware.checkUserExists, userController.createUser);
+router.put('/updateUser/:id', authMiddleware.authenticateToken, userMiddleware.checkRequiredFieldsToRegister, userController.updateUser);
 
 // produtos
-router.get('/product', productController.getAll);
-router.post('/registerProduct', productMiddleware.checkRequiredFieldsToRegister, productController.createProcut);
-router.put('/updateProduct/:id', productMiddleware.checkRequiredFieldsToRegister, productController.updateProduct);
+router.get('/product', authMiddleware.authenticateToken, productController.getAll);
+router.post('/registerProduct', authMiddleware.authenticateToken, productMiddleware.checkRequiredFieldsToRegister, productController.createProcut);
+router.put('/updateProduct/:id', authMiddleware.authenticateToken, productMiddleware.checkRequiredFieldsToRegister, productController.updateProduct);
 
 // indicacao
-router.get('/indication', indicationController.getAll);
-router.post('/registerIndication', indicationMiddleware.checkRequiredFieldsToRegister, indicationMiddleware.checkIndicationExists, indicationController.createIndication);
+router.get('/indication', authMiddleware.authenticateToken, indicationController.getAll);
+router.post('/registerIndication', authMiddleware.authenticateToken, indicationMiddleware.checkRequiredFieldsToRegister, indicationMiddleware.checkIndicationExists, indicationController.createIndication);
 
 // compra
-router.get('/sale/:userId', saleController.getAllByUserId);
-router.post('/newSale', saleMiddleware.checkRequiredFieldsToRegister, saleController.makeSale);
+router.get('/sale/:userId', authMiddleware.authenticateToken, saleController.getAllByUserId);
+router.post('/newSale', authMiddleware.authenticateToken, saleMiddleware.checkRequiredFieldsToRegister, saleController.makeSale);
 
 module.exports = router;
